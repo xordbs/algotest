fa29b7e1834c7397a0bfa15e6a09cfd898923368
인주비 <thetruthrunswild@naver.com>
2021-08-23 11:56:59 +0000

    시간초과

---
+++ 3주차/인주비/Main_BOJ_5639_이진검색트리_인주비.java
@@ -0,0 +1,38 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_BOJ_5639_이진검색트리_인주비 {
+	static int tree[];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = null;
+		tree = new int[10001];
+		int index = 0;
+		while ((str = br.readLine()) != null) {
+			tree[++index] = Integer.parseInt(str);
+		}
+		postOrder(1, index);
+	}  // end of main
+
+	// 왼쪽은 무조건 작고 오른쪽은 무조건 크다
+	// 왼쪽트리와 오른쪽트리의 중간을 찾아야 함 -> 자기보다 큰 원소를 만나기 전까지
+	public static void postOrder(int current, int lastIndex) {
+		// 기저조건은 current가 lastIndex를 넘어버리면?
+		if (current > lastIndex) return;
+		
+		int base = current+1;
+		while (current <= lastIndex 
+				|| tree[current] > tree[base] 
+						|| base <= lastIndex)
+			base++;
+		
+		// 찾았으면 그 앞의 배열, 즉 왼쪽 트리 안에서 또 후위순회
+		postOrder(current+1, base-1);
+		// 남은 오른쪽 안에서 후위순회
+		postOrder(base, lastIndex);
+		System.out.println(tree[current]);
+			
+	}
+}
