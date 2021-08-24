bd5930a55ef2482639487989d775f081da55a8d9
인주비 <thetruthrunswild@naver.com>
2021-08-24 04:50:20 +0000

    Upload New File

---
+++ 3주차/인주비/Main_BOJ_14888_연산자끼워넣기_인주비.java
@@ -0,0 +1,75 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_14888_연산자끼워넣기_인주비 {
+	/**
+	 124ms 
+	 */
+	
+	static int N;
+	static int[] input;
+	static int[] ops;
+	static int[] pickOps; // 선택된 선택자
+	static int max, min;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		input = new int[N];
+
+
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		ops = new int[4];
+
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < 4; i++) {
+			ops[i] = Integer.parseInt(st.nextToken());
+		}
+		pickOps = new int[N - 1];
+		max = Integer.MIN_VALUE;
+		min = Integer.MAX_VALUE;
+		
+		pick(0, input[0]);
+		
+		System.out.println(max);
+		System.out.println(min);
+	}
+
+	public static void pick(int cnt, int res) {
+		if (cnt == N - 1) {
+			max = Math.max(max, res);
+			min = Math.min(min, res);
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			if (ops[i] > 0) {
+				ops[i]--; // 하나 선택
+				pick(cnt + 1, calculate(cnt, i, res));
+				ops[i]++; // 다시 복구
+			}
+		}
+
+	}
+
+	public static int calculate(int cnt, int ops, int res) {
+		switch (ops) {
+		case 0:
+			return res + input[cnt + 1];
+		case 1:
+			return res - input[cnt + 1];
+		case 2:
+			return res * input[cnt + 1];
+		case 3:
+			return res / input[cnt + 1];
+		}
+		return 0;
+	}
+}
