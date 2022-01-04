ff50005c8a0f81e889c4e40938dc0704ae4e8bf5
인주비 <thetruthrunswild@naver.com>
2022-01-04 15:33:45 +0000

    Replace Main_백준_2240_자두나무_골드5_인주비.java

--- 15주차/인주비/Main_백준_2240_자두나무_골드5_인주비.java
+++ 15주차/인주비/Main_백준_2240_자두나무_골드5_인주비.java
@@ -29,15 +29,16 @@ public class Main_백준_2240_자두나무_골드5_인주비 {
 
 		for (int i = 1; i <= W; i++) {
 			for (int j = 1; j <= T; j++) {
-				if (i % 2 == num[j])
-					dp[i][j] = Math.max(dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1);
-				else
-					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j - 1]);
+				if (i % 2 == num[j]) // 현재 위치가 자두가 떨어지는 나무라면 
+					dp[i][j] = Math.max(dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1); // 움직이지 않고 +1, 움직여서 +1 중 최대값 구하기
+				else // 자두 떨어지지 않는다면
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j - 1]); // 움직이지 않거나 움직이거나 둘 중 하나 최대값 구하기
 			}
 		}
 
 		int answer = 0;
 		
+		// 움직이지 않고 최대일 수 있으므로 모든 움직임에 대해 최대값을 구해 본다
 		for (int i = 0; i <= W; i++) {
 			answer = Math.max(dp[i][T], answer);
 		}
